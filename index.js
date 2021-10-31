0aa7c548df25b30c2a151191b0239d8e66bd091d
Igoryakib <Igoryakib@gmail.com>
2021-10-31 20:36:14 +0200

    fix

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const onInput = () => {
+    alert('Це приклад інстаграм клону  не вводьте свої дані!!!!')
+};
+
+document.querySelectorAll('._2hvTZ').forEach(item => item.addEventListener('input', onInput));
